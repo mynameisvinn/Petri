17b46387b62d2f22dd4102cb2c8ab033d9af4b9b
mynameisvinn <vin.tang@gmail.com>
2020-06-24 14:52:19 -0400

    factor out create_wind

--- Petri/initialize_world.py
+++ Petri/initialize_world.py
@@ -4,6 +4,24 @@ import numpy as np
 
 from .utils import _draw, _return_random_loc, _sample
 
+
+def create_wind(n_rows, n_cols, n_force, n_kites):
+    world = {}
+    world['n_rows'] = n_rows
+    world['n_cols'] = n_cols
+            
+    for _ in range(n_force):
+        i = random.choice(np.arange(n_rows))
+        j = random.choice(np.arange(n_cols - (n_cols // 5), n_cols))  # the right fifth of the board
+        loc = (i, j)
+        world[loc] = (7, None)
+        
+    for _ in range(n_kites):
+        loc = _return_random_loc(n_rows, n_cols)
+        world[loc] = (1, None)    
+    return world
+
+
 def create_oil_world(n_rows, n_cols, n_dregs, n_oil):
     world = {}
     world['n_rows'] = n_rows
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # petri
 petri is a python environment to create and experiment with cellular automata. it inspired by david ackley's work on robust computing via artificial life.
+
+## use
+```python
+python setup.py develop
+```
